aa0a0d53f147c9d9fd2d0b56cf3aae1e0e09bd8f
khaki-njm <cornet19970121@gmail.com>
2025-06-12 21:25:01 +0900

    READMEにコンセプトを記載

--- hanashi_ai/lib/main.dart
+++ hanashi_ai/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +27,21 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          // ライトテーマ
+          brightness: Brightness.light,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          // ダークテーマ
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system, // Use system theme mode
+      home: const MyHomePage(title: 'Hanashi AI'),
     );
   }
 }
